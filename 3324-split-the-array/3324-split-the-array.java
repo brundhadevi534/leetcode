144db124b6d50aa2f0875a9b4a2b252184e876b7
J. Brundhadevi <jthaya6@gmail.com>
2025-10-27 23:04:42 +0530

    Time: 3 ms (23.57%), Space: 42.3 MB (97.93%) - LeetHub

---
+++ 3324-split-the-array/3324-split-the-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+        Arrays.sort(nums);
+    int count=1;int i;
+    for(i=1;i<nums.length;i++){
+        if(nums[i]==nums[i-1]){
+            count++;
+        if(count>2){
+        return false;
+        }
+        }
+        else{
+            count=1;
+        }
+    }
+    return true;
+    }
+}
